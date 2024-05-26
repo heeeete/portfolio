31b01101b3f9088adda6a9facdf6736aff8173c0
heeeete <mkoui@naver.com>
2024-05-26 18:46:45 +0900

    Add sitemap.xml, robots.txt

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+	siteUrl: "https://heeeete.github.io/portfolio",
+	generateRobotsTxt: true, // (옵션) 로봇 파일도 생성하려면 true로 설정
+	sitemapSize: 7000,
+	outDir: "./public", // 사이트맵 파일이 저장될 디렉토리
+};
