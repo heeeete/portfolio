d5cb14643a7986da6287c4ff6061bea5fb3cb09f
heeeete <mkoui@naver.com>
2024-05-30 01:20:03 +0900

    Updates

---
+++ _next/static/j3_BOwgPN60SJIru1-zph/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/15-d3228cd7ceedd219.js","static/css/4fa2444544b962df.css","static/chunks/pages/index-32136824304fdf27.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
